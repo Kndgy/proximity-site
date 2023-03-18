55b836c01e008c9ceb1e2d0a03ddca6b0268d183
kaine <ghanirafli8@gmail.com>
2023-03-19 02:55:07 +0700

    log x y locations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import './App.css'
 
+interface PlaneProps {
+  totalGrids: number;
+}
+
+function Plane({totalGrids}:PlaneProps){
+
+  const handleGridClick = (row: number, col: number) => {
+    console.log(`Clicked on grid row ${row}, col ${col}`);
+  };
+
+  const handleGridHover = (row: number, col: number) => {
+    console.log(`Hovering over grid row ${row}, col ${col}`);
+  };
+
+  const grids = [];
+  for (let i = 0; i < totalGrids; i++) {
+    const row = Math.floor(i / 10);
+    const col = i % 10;
+    grids.push(<div
+      onClick={() => handleGridClick(row, col)}
+      onMouseEnter={() => handleGridHover(row, col)}
+      className='Plane'>a</div>);
+  }
+
+  return (
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 100px)' }}>
+      {grids}
+    </div>
+  );
+}
+
 function App() {
 
   return (
     <div className="App">
-      h
+      <Plane totalGrids={100}/>
     </div>
   )
 }
