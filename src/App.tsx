372888aaba9d0e731cf851adb59e900ab8e80d33
kaine <ghanirafli8@gmail.com>
2023-03-19 03:11:02 +0700

    add actor to be moved in grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 
 interface PlaneProps {
@@ -10,9 +11,9 @@ function Plane({totalGrids}:PlaneProps){
     console.log(`Clicked on grid row ${row}, col ${col}`);
   };
 
-  const handleGridHover = (row: number, col: number) => {
-    console.log(`Hovering over grid row ${row}, col ${col}`);
-  };
+  // const handleGridHover = (row: number, col: number) => {
+  //   console.log(`Hovering over grid row ${row}, col ${col}`);
+  // };
 
   const grids = [];
   for (let i = 0; i < totalGrids; i++) {
@@ -20,22 +21,63 @@ function Plane({totalGrids}:PlaneProps){
     const col = i % 10;
     grids.push(<div
       onClick={() => handleGridClick(row, col)}
-      onMouseEnter={() => handleGridHover(row, col)}
-      className='Plane'>a</div>);
+      // onMouseEnter={() => handleGridHover(row, col)}
+      className='Plane'
+      key={i}
+      >a</div>);
   }
 
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 100px)' }}>
+      <>
       {grids}
+      </>
+  );
+}
+
+interface ActorProps {
+  initialRow: number;
+  initialCol: number;
+  onMove?: (row: number, col: number) => void;
+}
+
+function Actor({ initialRow, initialCol, onMove }:ActorProps){
+  const [row, setRow] = useState(initialRow);
+  const [col, setCol] = useState(initialCol);
+
+  const handleMove = (rowDelta: number, colDelta: number) => {
+    const newRow = row + rowDelta;
+    const newCol = col + colDelta;
+    // setRow(newRow);
+    // setCol(newCol);
+    console.log("clicked, should add event now")
+    // onMove(newRow, newCol);
+  };
+
+  return (
+    <div
+      className='Actor'
+      style={{ gridColumn: col + 1, gridRow: row + 1 }}
+      onClick={() => handleMove(0, 1)}
+    >
+      Actor
     </div>
   );
 }
 
 function App() {
+  const [actorPosition, setActorPosition] = useState({ row: 0, col: 0 });
+
+  const handleOnMove = (row:number, col: number) =>{
+    setActorPosition({row:1, col: 1})
+  }
 
   return (
     <div className="App">
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 100px)' }}>
+      <Actor initialCol={3} initialRow={2} onMove={(e)=>e}/>      
       <Plane totalGrids={100}/>
+      </div>
+
     </div>
   )
 }
