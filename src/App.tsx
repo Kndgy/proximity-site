671676346ce1cb93de441d6fa1303a5d81e5e657
kaine <ghanirafli8@gmail.com>
2023-03-19 07:13:50 +0700

    simple working implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 import './App.css'
 
 interface PlaneProps {
@@ -8,7 +9,7 @@ interface PlaneProps {
 function Plane({totalGrids}:PlaneProps){
 
   const handleGridClick = (row: number, col: number) => {
-    console.log(`Clicked on grid row ${row}, col ${col}`);
+    // console.log(`Clicked on grid row ${row}, col ${col}`);
   };
 
   // const handleGridHover = (row: number, col: number) => {
@@ -24,7 +25,7 @@ function Plane({totalGrids}:PlaneProps){
       // onMouseEnter={() => handleGridHover(row, col)}
       className='Plane'
       key={i}
-      >a</div>);
+      >{i}</div>);
   }
 
   return (
@@ -38,46 +39,82 @@ interface ActorProps {
   initialRow: number;
   initialCol: number;
   onMove?: (row: number, col: number) => void;
+  embed?: ReactNode
 }
 
-function Actor({ initialRow, initialCol, onMove }:ActorProps){
-  const [row, setRow] = useState(initialRow);
-  const [col, setCol] = useState(initialCol);
-
-  const handleMove = (rowDelta: number, colDelta: number) => {
-    const newRow = row + rowDelta;
-    const newCol = col + colDelta;
-    // setRow(newRow);
-    // setCol(newCol);
-    console.log("clicked, should add event now")
-    // onMove(newRow, newCol);
-  };
-
+function Actor({ initialRow, initialCol, embed, onMove }:ActorProps){
   return (
     <div
       className='Actor'
-      style={{ gridColumn: col + 1, gridRow: row + 1 }}
-      onClick={() => handleMove(0, 1)}
+      style={{ gridColumn: initialCol + 1, gridRow: initialRow + 1 }}
     >
-      Actor
+      {embed}
     </div>
   );
 }
 
 function App() {
-  const [actorPosition, setActorPosition] = useState({ row: 0, col: 0 });
+  const [playerPosition, setPlayerPosition] = useState({ row: 0, col: 0 });
+  const [actorPosition, setActorPosition] = useState({ row: 2, col: 2 });
 
-  const handleOnMove = (row:number, col: number) =>{
-    setActorPosition({row:1, col: 1})
+  const handleBottomMove = useCallback(() => {
+    setActorPosition(prevPosition => prevPosition.row < 4 ? ({col: prevPosition.col, row: prevPosition.row+1}) : prevPosition);
+  }, []);
+  
+  
+  const handleTopMove = useCallback(() => {
+    setActorPosition(prevPosition => prevPosition.row >= 1 ? {col: prevPosition.col, row: prevPosition.row-1} : prevPosition);
+  }, []);
+  
+  
+  const handleRightMove = useCallback(() => {
+    setActorPosition(prevPosition => prevPosition.col<11 ? ({col:prevPosition.col+1 , row:prevPosition.row}) : prevPosition);
+  }, []);
+  
+  const handleLeftMove = useCallback(() => {
+    setActorPosition(prevPosition => prevPosition.col >= 1 ? {col: prevPosition.col-1, row: prevPosition.row} : prevPosition);
+  }, []);
+  
+
+  console.log(actorPosition)
+  console.log(1 - Math.min(Math.max((actorPosition.row + actorPosition.col) / 10, 0), 1));
+
+
+
+  const controlAudio = () =>{
+    return 1 - Math.min(Math.max((actorPosition.row + actorPosition.col) / 10, 0), 1)
   }
+
+  controlAudio()
+
 
   return (
     <div className="App">
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 100px)' }}>
-      <Actor initialCol={3} initialRow={2} onMove={(e)=>e}/>      
-      <Plane totalGrids={100}/>
+      proximity audio on site, because why not lol
+      <br/>
+      <br/>
+      move player to hear the effect
+      <div className='buttons'> 
+        <button onClick={handleLeftMove} >Left</button> 
+        <button onClick={handleTopMove}>Top</button> 
+        <button onClick={handleBottomMove}>Bottom</button> 
+        <button onClick={handleRightMove}>Right</button> 
       </div>
-
+      click 3 dot to start play
+      {/* actor player
+      <div> 
+        <button>Left</button> 
+        <button>Right</button> 
+        <button>Top</button> 
+        <button>Bottom</button> </div> */}
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(12, 100px)', paddingTop:'40px' }}>
+        <Actor initialCol={0} initialRow={0} embed={
+          <><ReactAudioPlayer volume={controlAudio()} src='/ballin.mp3' loop={true} autoPlay={true} controls/></>
+        }/>
+        <Actor initialCol={actorPosition.col} initialRow={actorPosition.row} embed={"player"}/>
+        <Plane totalGrids={58}/>
+      </div>
+      {/* a */}
     </div>
   )
 }
